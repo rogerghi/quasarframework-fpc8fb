6cf8aebba2bda72754da6f66bacc2eb8b2a4a7d2
rogerghi <56428285+rogerghi@users.noreply.github.com>
2022-09-27 16:04:13 +0700

    1 file modified

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -112,51 +112,6 @@
 import { defineComponent, ref } from 'vue';
 // import EssentialLink from 'components/EssentialLink.vue';
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev',
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework',
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev',
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev',
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
-  },
-];
-
 export default defineComponent({
   name: 'MainLayout',
 
@@ -167,7 +122,7 @@ export default defineComponent({
   setup() {
     return {
       left: false,
-      text: '',
+      text: 'test',
       files: null,
       textHal: 'Mohon Sekiranya dapat di setujui. Terimakasih',
       right: false,
