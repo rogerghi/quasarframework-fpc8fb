c14da089a01774173ab6e9f7c919d031c0c2e591
rogerghi <56428285+rogerghi@users.noreply.github.com>
2022-09-27 15:16:29 +0700

    1 file modified

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,52 +1,111 @@
 <template>
-  <q-page class="column flex-center">
-    <q-knob
-      v-model="count"
-      :min="min"
-      :max="max"
-      size="80px"
-      show-value
-      :thickness="0.13"
-      color="primary"
-      track-color="dark"
-    >
-      <q-avatar size="75px">
-        <img alt="Quasar logo" src="~assets/quasar-logo-inner.svg" />
-      </q-avatar>
-    </q-knob>
+  <q-layout view="hHh lpR fFf">
+    <q-header flat class="bg-white text-black">
+      <q-toolbar>
+        <q-toolbar-title class="text-weight-light" style="padding-left: 5px">
+          Form Persetujuan
+        </q-toolbar-title>
 
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 140px"
-    />
+        <q-space></q-space>
 
-    <div class="q-mt-xl">
-      <q-btn
-        color="primary"
-        dense
-        round
-        label="-"
-        :disable="count === min"
-        @click="count--"
-      />
+        <div class="q-pa-md q-gutter-sm">
+          <q-btn icon="mdi-keyboard-return" color="red" label="Return"></q-btn>
+          <q-btn
+            icon="mdi-account-reactivate"
+            color="orange"
+            label="Disposition"
+          ></q-btn>
+          <q-btn icon="mdi-content-save" color="blue" label="Save"></q-btn>
+          <q-btn icon="mdi-send" color="teal" label="Send"></q-btn>
+        </div>
+      </q-toolbar>
+    </q-header>
 
-      <span class="q-mx-md text-bold">{{ count }}</span>
+    <q-drawer show-if-above v-model="left" side="left"> </q-drawer>
 
-      <q-btn
-        color="primary"
-        dense
-        round
-        label="+"
-        :disable="count === max"
-        @click="count++"
-      />
-    </div>
+    <q-drawer show-if-above v-model="right" side="right"> </q-drawer>
 
-    <div class="q-mt-md" style="width: 200px">
-      <q-slider v-model="count" :min="min" :max="max" />
-    </div>
-  </q-page>
+    <q-page-container>
+      <router-view>
+        <div class="row">
+          <div class="col">
+            <div class="q-gutter-md q-pt-md q-pb-lg">
+              <q-input
+                filled
+                v-model="name"
+                label="Nomor Surat"
+                hint="Mohon diisi dengan nomor surat dari pimpinan"
+              ></q-input>
+
+              <q-input
+                filled
+                v-model="date"
+                mask="date"
+                :rules="['date']"
+                label="Tanggal Surat"
+                hint="Mohon diisi dengan tanggal dari surat pimpinan"
+              >
+                <template v-slot:append>
+                  <q-icon name="event" class="cursor-pointer">
+                    <q-popup-proxy
+                      ref="qDateProxy"
+                      transition-show="scale"
+                      transition-hide="scale"
+                    >
+                      <q-date v-model="date">
+                        <div class="row items-center justify-end">
+                          <q-btn
+                            v-close-popup
+                            label="Close"
+                            color="primary"
+                            flat
+                          ></q-btn>
+                        </div>
+                      </q-date>
+                    </q-popup-proxy>
+                  </q-icon>
+                </template>
+              </q-input>
+
+              <q-input
+                v-model="text"
+                filled
+                type="textarea"
+                label="Hal"
+                placeholder="Ketik disini.."
+                hint="Mohon diisi sesuai dengan tujuan pengajuan ini"
+              ></q-input>
+            </div>
+          </div>
+          <div class="col">
+            <div class="q-pa-md">
+              <div class="q-gutter-md items-start">
+                <q-file
+                  v-model="files"
+                  label="Lampiran surat persetujuan"
+                  filled
+                  counter
+                  :counter-label="counterLabelFn"
+                  max-files="1"
+                  multiple
+                >
+                  <template v-slot:prepend>
+                    <q-icon name="attach_file"></q-icon>
+                  </template>
+                </q-file>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <q-toggle
+            v-model="accept"
+            label="Saya menyetujui, bahwa data informasi dan lampiran tersebut sudah benar."
+          ></q-toggle>
+        </div>
+      </router-view>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script setup>
